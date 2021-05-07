688bdc61e8b4066cfa703d905abfddddcfdbeb70
ValentinLHB <valentin.lhb@gmail.com>
2021-05-07 11:54:20 +0200

    algorithmie

---
+++ scripts/divsionBy7.js
@@ -0,0 +1,14 @@
+var affichage = function () {
+
+    var limite = document.getElementById("ex2-user-input").value;
+    
+    for (let i = 1; i <= limite; i++) {
+        if (i%7 == 0) {
+            document.getElementById('demande2').innerHTML += i + ' est divisible par 7'+  '<br>' ;
+        }
+        
+    }
+
+};
+
+document.getElementById("division-by-7").addEventListener("click", affichage, false);
