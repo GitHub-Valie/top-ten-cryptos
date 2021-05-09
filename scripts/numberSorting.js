17515f47dbb07cd0db35994ff08b24e66be2ded6
ValentinLHB <valentin.lhb@gmail.com>
2021-05-09 22:05:26 +0200

    minor style changes

---
+++ scripts/numberSorting.js
@@ -0,0 +1,37 @@
+// Trie 3 nombres saisis par le user
+
+var affichage = function () {
+
+    var inputNumber1 = document.getElementById("ex7-user-input1").value;
+    var inputNumber2 = document.getElementById("ex7-user-input2").value;
+    var inputNumber3 = document.getElementById("ex7-user-input3").value;
+
+    if (inputNumber1 > inputNumber2) { // inputNumber1 > inputNumber2
+        if (inputNumber1 > inputNumber3) { // inputNumber 1 > inputNumber2 & inputNumber3
+            document.getElementById('demande7').innerHTML += inputNumber1 + " est plus grand que ";
+            if (inputNumber2 > inputNumber3) { // inputNumber 1 > inputNumber2 > inputNumber3
+                document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus grand que " + inputNumber3;
+            }
+            else { // inputNumber 1 > inputNumber2 < inputNumber3
+                document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus petit que " + inputNumber3;
+            }
+        }
+        else { // inputNumber3 > inputNumber1 > inputNumber2
+            document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus petit que " + inputNumber3;
+        }
+    } else { // inputNumber1 < inputNumber2
+        document.getElementById('demande7').innerHTML += inputNumber1 + " est plus petit que " + inputNumber2;
+        if (inputNumber2 > inputNumber3) { // inputNumber2 > inputNumber3 & inputNumber2 > inputNumber1
+            if (inputNumber3 > inputNumber1) { // inputNumber1 < inputNumber3 < inputNumber2
+                document.getElementById('demande7').innerHTML += " qui est plus grand que " + inputNumber3;
+            } else { // inputNumber3 < inputNumber1 < inputNumber2
+                document.getElementById('demande7').innerHTML += " qui est plus grand que " + inputNumber3;
+            }
+        } else { // inputNumber1 < inputNumber2 < inputNumber3
+            document.getElementById('demande7').innerHTML += " qui est plus petit que " + inputNumber3;
+        }
+    }
+    document.getElementById('demande7').innerHTML += '<br>';
+};
+
+document.getElementById("sort-numbers").addEventListener("click", affichage, false);
