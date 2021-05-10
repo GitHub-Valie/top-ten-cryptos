98e6ebe62dd6537bd5d78b8200b5e07e63ee1497
ValentinLHB <valentin.lhb@gmail.com>
2021-05-10 09:03:05 +0200

    fixed numberSorting

--- scripts/numberSorting.js
+++ scripts/numberSorting.js
@@ -11,13 +11,11 @@ var affichage = function () {
             document.getElementById('demande7').innerHTML += inputNumber1 + " est plus grand que ";
             if (inputNumber2 > inputNumber3) { // inputNumber 1 > inputNumber2 > inputNumber3
                 document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus grand que " + inputNumber3;
-            }
-            else { // inputNumber 1 > inputNumber2 < inputNumber3
+            } else { // inputNumber 1 > inputNumber2 < inputNumber3
                 document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus petit que " + inputNumber3;
             }
-        }
-        else { // inputNumber3 > inputNumber1 > inputNumber2
-            document.getElementById('demande7').innerHTML += inputNumber2 + " qui est plus petit que " + inputNumber3;
+        } else { // inputNumber3 > inputNumber1 > inputNumber2
+            document.getElementById('demande7').innerHTML += inputNumber1 + " est plus grand que " + inputNumber2 + " qui est plus petit que " + inputNumber3;
         }
     } else { // inputNumber1 < inputNumber2
         document.getElementById('demande7').innerHTML += inputNumber1 + " est plus petit que " + inputNumber2;
